6bed27af792fa26c58fba5fa8bb66cafb663f4be
Jeremiah <jsnidar@gmail.com>
2021-11-23 11:19:26 -0600

    commit tree 2b4ebedca4fec1b743817a62ea8937a7ad8098de

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,23 @@
+class PlantsController < ApplicationController
+
+  def index
+    plants = Plant.all 
+    render json: plants, only: [:id, :name, :image, :price] 
+  end
+
+  def show 
+    plant = Plant.find(params[:id])
+    render json: plant, only: [:id, :name, :image, :price]
+  end
+
+  def create
+    plant = Plant.create(plant_params)
+    render json: plant, only: [:id, :name, :image, :price]
+  end
+
+  private
+
+  def plant_params
+    params.permit(:name, :image, :price)
+  end
+end
